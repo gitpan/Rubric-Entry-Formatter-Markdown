aa24dcc4d13d6336997725e578feb67643484dcf
Ricardo SIGNES <rjbs@cpan.org>
2012-02-24 13:18:24 +0000

    Import of RJBS/Rubric-Entry-Formatter-Markdown-0.554 from CPAN.
    gitpan-cpan-distribution: Rubric-Entry-Formatter-Markdown
    gitpan-cpan-version:      0.554
    gitpan-cpan-path:         RJBS/Rubric-Entry-Formatter-Markdown-0.554.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/Rubric/Entry/Formatter/Markdown.pm
+++ lib/Rubric/Entry/Formatter/Markdown.pm
@@ -2,36 +2,24 @@ use strict;
 use warnings;
 
 package Rubric::Entry::Formatter::Markdown;
-our $VERSION = '0.553';
+{
+  $Rubric::Entry::Formatter::Markdown::VERSION = '0.554';
+}
+# ABSTRACT: format entries with Markdown (duh!)
 
-=head1 NAME
 
-Rubric::Entry::Formatter::Markdown - format entries with Markdown (duh!)
+use Text::Markdown ();
+use Text::MultiMarkdown ();
 
-=head1 VERSION
-
-version 0.553
-
-=head1 DESCRIPTION
-
-This formatter will use Markdown (specifically, Text::Markdown) to format
-entries into HTML.
-
-=cut
-
-use Text::Markdown;
-
-=head1 METHODS
-
-=head2 C< as_html >
-
-=head2 C< as_text >
-
-=cut
 
 sub as_html {
   my ($class, $arg, $config) = @_;
-  return Text::Markdown->new(%$config)->markdown($arg->{text});
+  my %config = %$config;
+  my $md = (delete $config{multimarkdown})
+         ? 'Text::MultiMarkdown'
+         : 'Text::Markdown';
+
+  return $md->new(%$config)->markdown($arg->{text});
 }
 
 sub as_text {
@@ -40,24 +28,44 @@ sub as_text {
   return $arg->{text};
 }
 
-=head1 TODO
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Rubric::Entry::Formatter::Markdown - format entries with Markdown (duh!)
+
+=head1 VERSION
+
+version 0.554
+
+=head1 DESCRIPTION
+
+This formatter will use Markdown (specifically, Text::Markdown) to format
+entries into HTML.
+
+Configuration for the formatter is given to the Text::Markdown constructor,
+with the exception of the C<multimarkdown> option.  If given and true, it will
+cause the formatter to use L<Text::MultiMarkdown> instead of L<Text::Markdown>.
+
+=head1 METHODS
+
+=head2 as_html
+
+=head2 as_text
 
 =head1 AUTHOR
 
-Ricardo SIGNES, C<< <rjbs@cpan.org> >>
+Ricardo SIGNES <rjbs@cpan.org>
 
-=head1 BUGS
+=head1 COPYRIGHT AND LICENSE
 
-Please report any bugs or feature requests to C<bug-rubric@rt.cpan.org>, or
-through the web interface at L<http://rt.cpan.org>. I will be notified, and
-then you'll automatically be notified of progress on your bug as I make
-changes.
+This software is copyright (c) 2005 by Ricardo SIGNES.
 
-=head1 COPYRIGHT
-
-Copyright 2005-2006 Ricardo SIGNES.  This program is free software;  you can
-redistribute it and/or modify it under the same terms as Perl itself.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
 
-1;
